9c6546949e582e0b7e857626e38efd066619984f
xhyan <imissyou.xhyan@gmail.com>
2016-08-19 18:20:51 +0800

    antd Form 实现

---
+++ src/routes/antd/Form/components/Form.jsx
@@ -0,0 +1,40 @@
+import {
+  Form,
+  Select,
+  InputNumber,
+  DatePicker,
+  TimePicker,
+  Switch,
+  Radio,
+  Cascader,
+  Slider,
+  Button,
+  Col,
+  Upload,
+  Icon
+} from 'antd';
+import React, {Component} from 'react';
+
+const FormItem = Form.Item;
+const Option = Select.Option;
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
+
+const areaData = [{
+  value: 'shanghai',
+  label: '上海',
+  children: [{
+    value: 'shanghaishi',
+    label: '上海市',
+    children: [{
+      value: 'pudongxinqu',
+      label: '浦东新区',
+    }],
+  }],
+}];
+
+class AntForm extends Component{
+  render(){
+
+  }
+}
